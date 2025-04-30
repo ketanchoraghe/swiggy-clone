27d3bfc80a112e2f79d8a37a9a63efdad130c2b1
Follow Ur Dreams <ketanchoraghe2004@gmail.com>
2025-04-30 23:25:56 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 57596b73f249c68992d05ee2884aaaf52d9564ca
+  base: '/swiggy-clone/', 
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
