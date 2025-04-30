84aedfc75b643a14403b7bc211a2098987344e26
Follow Ur Dreams <ketanchoraghe2004@gmail.com>
2025-05-01 02:36:31 +0530

    Add source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,29 @@
-import React from 'react'
-<<<<<<< HEAD
-import Header from './Component/Header'
-import Category from './Component/Category'
-import TopRest from './Component/TopRest'
-import OnlineDelivery from './Component/OnlineDelivery'
-=======
-import Navbar from './Components/Navbar'
-import Header from './Components/Header'
-import Feature from './Components/Feature'
-import Offer from './Components/Offer'
-import About from './Components/About'
-import Contact from './Components/Contact'
->>>>>>> 57596b73f249c68992d05ee2884aaaf52d9564ca
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Feature from './Components/Feature';
+import Offer from './Components/Offer';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
+import Category from './Component/Category';
+import TopRest from './Component/TopRest';
+import OnlineDelivery from './Component/OnlineDelivery';
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <>
-    <Header/>
-    <Category/>
-    <TopRest/>
-    <OnlineDelivery/>
+      <Navbar />
+      <Header />
+      <Feature />
+      <Offer />
+      <About />
+      <Contact />
+      <Category />
+      <TopRest />
+      <OnlineDelivery />
     </>
-=======
-  <>
- < Navbar/>
- < Header/>
- <Feature/>
- <Offer/>
- <About/>
- <Contact/>
- </>
->>>>>>> 57596b73f249c68992d05ee2884aaaf52d9564ca
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-=======
-import React, { StrictMode } from 'react'
->>>>>>> 57596b73f249c68992d05ee2884aaaf52d9564ca
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
   <StrictMode>
     <App />
-  </StrictMode>,
-=======
-  <React.StrictMode>
-    <App />
-    </React.StrictMode>
-  
->>>>>>> 57596b73f249c68992d05ee2884aaaf52d9564ca
-)
+  </StrictMode>
+);
